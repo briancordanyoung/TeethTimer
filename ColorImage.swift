933fc14722e039923eb34db67fca7f6707525917
Brian Young <bdy-github@hiddentower.com>
2015-06-08 07:16:34 -0700

    formatting
    Former-commit-id: e09795684a6394c965788fb0bc6b1125b3b3eb36

--- ColorImage.swift
+++ ColorImage.swift
@@ -2,36 +2,30 @@ import UIKit
 
 final class ColorImage: NSObject {
     
-    class func colorizeImage(image: UIImage, withColor color: UIColor) -> UIImage {
-        UIGraphicsBeginImageContext(image.size)
-        let context = UIGraphicsGetCurrentContext()
+  class func colorizeImage(image: UIImage, withColor color: UIColor) -> UIImage {
+    UIGraphicsBeginImageContext(image.size)
+    let context = UIGraphicsGetCurrentContext()
 
-        let area = CGRectMake(0, 0, image.size.width, image.size.height)
+    let area = CGRectMake(0, 0, image.size.width, image.size.height)
 
-        CGContextScaleCTM(context, 1, -1)
+    CGContextScaleCTM(context, 1, -1)
+    CGContextTranslateCTM(context, 0, -area.size.height)
+    CGContextSaveGState(context)
+    CGContextClipToMask(context, area, image.CGImage)
 
-        CGContextTranslateCTM(context, 0, -area.size.height)
+    color.set()
 
-        CGContextSaveGState(context)
+    CGContextFillRect(context, area)
+    CGContextRestoreGState(context)
+    CGContextSetBlendMode(context, kCGBlendModeMultiply)
+    CGContextDrawImage(context, area, image.CGImage)
 
-        CGContextClipToMask(context, area, image.CGImage)
+    let colorizedImage = UIGraphicsGetImageFromCurrentImageContext()
 
-        color.set()
+    UIGraphicsEndImageContext()
 
-        CGContextFillRect(context, area)
-
-        CGContextRestoreGState(context)
-
-        CGContextSetBlendMode(context, kCGBlendModeMultiply)
-
-        CGContextDrawImage(context, area, image.CGImage)
-
-        let colorizedImage = UIGraphicsGetImageFromCurrentImageContext()
-
-        UIGraphicsEndImageContext()
-
-        return colorizedImage
-    }
-    
+    return colorizedImage
+  }
+  
     
 }
