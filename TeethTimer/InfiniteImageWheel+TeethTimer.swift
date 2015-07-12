4aa39d6a7bb44a451728599bada4130ac5df55e5
Brian Young <bdy-github@hiddentower.com>
2015-07-11 20:41:52 -0700

    removed rotationForIndex and re-implimented it in RotationState as wedgeCenterForIndex

--- TeethTimer/InfiniteImageWheel+TeethTimer.swift
+++ TeethTimer/InfiniteImageWheel+TeethTimer.swift
@@ -2,37 +2,6 @@ import UIKit
 
 extension InfiniteImageWheel {
   
-  func rotationForIndex(index: WedgeIndex) -> Rotation {
-    
-    let wedgeSeperation               = Rotation(wedgeSeries.wedgeSeperation)
-    let invertedIndex                 = rotationState.wedgeMaxIndex - index
-    let distanceOfCompletRotations    = rotationState.distanceOfCompletRotations
-
-    let distanceWithinPartialRotation: Rotation
-    
-    switch wedgeSeries.direction {
-    case .ClockwiseLayout:
-      distanceWithinPartialRotation = abs(wedgeSeperation * invertedIndex)
-    case .CounterClockwiseLayout:
-      distanceWithinPartialRotation = abs(wedgeSeperation * index)
-    }
-    
-    let distanceToWedgeCenter = distanceOfCompletRotations +
-                                distanceWithinPartialRotation
-
-    return polarityAdjustedRotation(distanceToWedgeCenter)
-  }
-  
-  func polarityAdjustedRotation(distanceToWedgeCenter: Rotation) -> Rotation {
-    switch rotationState.polarity {
-    case .Positive:
-      return abs(distanceToWedgeCenter)
-      
-    case .Negative:
-      return abs(distanceToWedgeCenter) * -1
-    }
-  }
-  
   func indexFromRotation(rotation: Rotation) -> WedgeIndex {
     let state = RotationState(rotation: rotation,
                            wedgeSeries: wedgeSeries)
